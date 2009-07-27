5e8bc5125b154ab0547d2eac05441151d99ca3e8
carns <carns@mcs.anl.gov>
2009-07-27 13:48:49 +0000

    install script for surveyor
    git-svn-id: https://svn.mcs.anl.gov/repos/darshan/trunk@44 3b7491f3-a168-0410-bf4b-c445ed680a29

---
+++ maint/install-surveyor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#PREFIX=/soft/apps/unsupported/darshan-1.1.0
+#LOGDIR=/pvfs-surveyor/darshan_logs
+PREFIX=/home/carns/unsupported/darshan-1.1.0
+LOGDIR=/home/carns/darshan_logs
+
+#darshan 
+cd ../
+./configure --with-mem-align=16 --with-log-path=$LOGDIR --prefix=$PREFIX --with-zlib-for-mpi=/soft/apps/zlib-1.2.3/ CFLAGS="-O2" && make && make install
+mkdir -p $LOGDIR
+$PREFIX/bin/darshan-mk-log-dirs.pl
+
