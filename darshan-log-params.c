63161d1ef72fc8e5d962ae55aa49444be2c8db8d
carns <carns@mcs.anl.gov>
2009-12-10 19:43:03 +0000

    added simple utility to print log format parameters
    git-svn-id: https://svn.mcs.anl.gov/repos/darshan/trunk@120 3b7491f3-a168-0410-bf4b-c445ed680a29

---
+++ darshan-log-params.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "darshan-log-format.h"
+
+int main(int argc, char **argv) 
+{
+    printf("version: %s\n", CP_VERSION);
+    printf("CP_JOB_RECORD_SIZE: %d\n", CP_JOB_RECORD_SIZE);
+    printf("CP_EXE_LEN: %d\n", CP_EXE_LEN);
+    printf("CP_FILE_RECORD_SIZE: %d\n", CP_FILE_RECORD_SIZE);
+    printf("CP_NAME_SUFFIX_LEN: %d\n", CP_NAME_SUFFIX_LEN);
+    printf("CP_NUM_INDICES: %d\n", CP_NUM_INDICES);
+    printf("CP_F_NUM_INDICES: %d\n", CP_F_NUM_INDICES);
+    return(0);
+}
