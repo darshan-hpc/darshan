91b5e41aa21e75280397471be95b5aa77ca2e3b9
harms <harms@alcf.anl.gov>
2010-07-28 20:50:53 +0000

    Updated compiler generation scripts to take a '--trim' option which will remove the compiler's absolute path if it has one.
    Modified Makefile so that objects are only rebuilt if the need to be.
    
    Modified bg compiler generation script to remove compiler paths from bg
    compilers and leave the absolute paths for gnu compilers.
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/darshan/branches/trac5@317 3b7491f3-a168-0410-bf4b-c445ed680a29

--- maint/generate-bg-compilers.sh
+++ maint/generate-bg-compilers.sh
@@ -3,16 +3,34 @@
 # Generates compiler scripts for all BG compilers
 #
 
+PREFIX=/home/harms/darshan/test
+#PREFIX=/soft/apps/darshan-2.0.0
 BGPATH=/bgsys/drivers/ppcfloor/comm/xxx/bin
-INSTALL=/soft/apps/darshan-2.0.0/bin
 
 for compiler_type in default fast xl;
 do
-   compiler_path=${BGPATH/xxx/$type}
+   compiler_path=${BGPATH/xxx/$compiler_type}
+   compiler_opt=""
    for compiler in $(ls $compiler_path);
    do
-       if [ $compiler != "mpich2version" -a $compiler != "parkill" ]; then
-           ../darshan-gen-cc.pl --output=$INSTALL/$compiler_type/$compiler $compiler_path/$compiler"
+       if [ $compiler != "mpich2version" -a \
+            $compiler != "parkill" ]; then
+           mkdir -p $PREFIX/bin/$compiler_type
+           if [ $(expr match $compiler ".*xl.*") -gt 0 -o \
+                $compiler_type = "fast" -o \
+                $compiler_type = "xl" ]; then
+               compiler_opt="--trim";
+           fi
+           if [ $(expr match $compiler ".*cxx") -gt 0 ]; then
+               $PREFIX/bin/darshan-gen-cxx.pl $compiler_opt --output=$PREFIX/bin/$compiler_type/$compiler $compiler_path/$compiler
+           elif [ $(expr match $compiler ".*f77") -gt 0 -o \
+                  $(expr match $compiler ".*f90") -gt 0 -o \
+                  $(expr match $compiler ".*f95") -gt 0 -o \
+                  $(expr match $compiler ".*f2003") -gt 0 ]; then
+               $PREFIX/bin/darshan-gen-fortran.pl $compiler_opt --output=$PREFIX/bin/$compiler_type/$compiler $compiler_path/$compiler
+           else
+               $PREFIX/bin/darshan-gen-cc.pl $compiler_opt --output=$PREFIX/bin/$compiler_type/$compiler $compiler_path/$compiler
+           fi
        fi
    done 
 done
