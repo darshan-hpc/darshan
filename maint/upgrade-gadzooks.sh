81415e5d979970015e130558c4636cd2cc991648
harms <harms@alcf.anl.gov>
2010-12-07 20:38:34 +0000

    Update the Eureka and Gadzooks to install the entire Darshan suite since everything is useable and nothing is BG specific anymore.
    git-svn-id: https://svn.mcs.anl.gov/repos/darshan/trunk@352 3b7491f3-a168-0410-bf4b-c445ed680a29

--- maint/upgrade-gadzooks.sh
+++ maint/upgrade-gadzooks.sh
@@ -3,23 +3,8 @@
 PREFIX=/soft/apps/darshan-2.0.2
 LOGDIR=/pvfs-surveyor/logs/darshan
 
-# darshan job summary
-cd ../
-install -d $PREFIX
-install -d $PREFIX/bin
-install -d $PREFIX/lib
-install -d $PREFIX/lib/TeX
-install -d $PREFIX/lib/Number
-install -d $PREFIX/lib/Number/Bytes
-install -d $PREFIX/share
-install -m 755 util/bin/darshan-job-summary.pl $PREFIX/bin/
-install -m 644 util/lib/TeX/Encode.pm $PREFIX/lib/TeX/
-install -m 644 util/lib/Number/Bytes/Human.pm $PREFIX/lib/Number/Bytes
-install -m 644 util/share/* $PREFIX/share/
-
-# we also need to build darshan-parser in 2.0.2
-./configure --with-mem-align=16 --with-log-path=$LOGDIR --prefix=$PREFIX --with-jobid-env=COBALT_JOBID CFLAGS="-O2" && make darshan-parser
-install -m 755 darshan-parser $PREFIX/bin/
+# Configure, Make and Install Darshan suite
+./configure --with-mem-align=16 --with-log-path=$LOGDIR --prefix=$PREFIX --with-jobid-env=COBALT_JOBID CFLAGS="-O2" && make && make install
 
 #gnuplot (new version with additional histogram support)
 cd extern
