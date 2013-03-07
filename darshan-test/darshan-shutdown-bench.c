6ec71672a7e1e94792192e6e8ebfaa95d9750b03
carns <carns@mcs.anl.gov>
2013-03-07 21:11:34 +0000

    remove unused "iterations" argument to benchmark
    git-svn-id: https://svn.mcs.anl.gov/repos/darshan/trunk@673 3b7491f3-a168-0410-bf4b-c445ed680a29

--- darshan-test/darshan-shutdown-bench.c
+++ darshan-test/darshan-shutdown-bench.c
@@ -26,32 +26,16 @@ int main(int argc, char **argv)
 {
     int nprocs;
     int mynod;
-    int ret;
-    int iters;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &mynod);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    /* parse argument for number of iterations */
-    if(argc != 2)
+    if(argc != 1)
     {
-        fprintf(stderr, "Usage: %s <number of iterations>\n", argv[0]);
+        fprintf(stderr, "Usage: %s\n", argv[0]);
         MPI_Finalize();
         return(-1);
-    }
-
-    ret = sscanf(argv[1], "%d", &iters);
-    if(ret != 1)
-    {
-        fprintf(stderr, "Usage: %s <number of iterations>\n", argv[0]);
-        MPI_Finalize();
-        return(-1);
-    }
-
-    if(mynod == 0)
-    {
-        printf("ignoring iters argument for now.\n");
     }
 
     darshan_shutdown_bench(argc, argv, mynod, nprocs);
