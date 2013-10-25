0e828ba78a5ff44dc57829b8f545288b85e0c403
carns <carns@mcs.anl.gov>
2013-10-25 16:11:46 +0000

    show nprocs too
    git-svn-id: https://svn.mcs.anl.gov/repos/darshan/trunk@796 3b7491f3-a168-0410-bf4b-c445ed680a29

--- darshan-test/darshan-anon-core-hours.pl
+++ darshan-test/darshan-anon-core-hours.pl
@@ -5,7 +5,7 @@
 
 # takes a single darshan log as input; expectation is that the log is from
 # the public data repo and has been annotated with compute node information.
-# Prints 3 columns: file name, compute nodes, and run time
+# Prints 4 columns: file name, num procs, compute nodes, and run time
 
 
 sub main()
@@ -14,6 +14,7 @@ sub main()
     my $header;
     my $cn;
     my $runtime;
+    my $nprocs;
 
     $logfile=$ARGV[0];
 
@@ -23,7 +24,10 @@ sub main()
         $cn = $1;
         if($header =~ /run time: (\d+)/){
             $runtime = $1;
-            print("$logfile\t$cn\t$runtime\n");
+            if($header =~ /nprocs: (\d+)/){
+                $nprocs = $1;
+                print("$logfile\t$nprocs\t$cn\t$runtime\n");
+            }
         }
     }
 
