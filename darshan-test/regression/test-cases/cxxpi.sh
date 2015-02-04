55e1f37ed5a377716789882ba2eeed0980fcfb0e
Phil Carns <carns@mcs.anl.gov>
2015-02-03 21:33:59 -0500

    experimenting with jenkins environment

--- darshan-test/regression/test-cases/cxxpi.sh
+++ darshan-test/regression/test-cases/cxxpi.sh
@@ -7,7 +7,7 @@ export DARSHAN_LOGFILE=$DARSHAN_TMP/${PROG}.darshan.gz
 rm -f ${DARSHAN_LOGFILE}
 
 # compile
-$DARSHAN_CXX $DARSHAN_TESTDIR/test-cases/src/${PROG}.cxx -o $DARSHAN_TMP/${PROG}
+$DARSHAN_CXX -static $DARSHAN_TESTDIR/test-cases/src/${PROG}.cxx -o $DARSHAN_TMP/${PROG}
 if [ $? -ne 0 ]; then
     echo "Error: failed to compile ${PROG}" 1>&2
     exit 1
