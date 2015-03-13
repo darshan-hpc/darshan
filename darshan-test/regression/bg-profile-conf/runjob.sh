3cd66a93b5f8d2cb6725e5302f64da8163e6adce
Phil Carns <carns@mcs.anl.gov>
2015-03-13 14:43:35 +0000

    bluegend profile testing environment

---
+++ darshan-test/regression/bg-profile-conf/runjob.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# submit job and get job id
+jobid=`qsub --mode c16 --proccount $DARSHAN_DEFAULT_NPROCS -A SSSPPg -t 10 -n 1 --output $DARSHAN_TMP/$$-tmp.out --error $DARSHAN_TMP/$$-tmp.err --debuglog $DARSHAN_TMP/$$-tmp.debuglog "$@"`
+if [ $? -ne 0 ]; then
+	echo "Error: failed to qsub $@"
+	exit 1
+fi
+
+output="foo"
+rc=0
+
+# loop as long as qstat succeeds and shows information about job
+while [ -n "$output" -a "$rc" -eq 0 ]; do
+	sleep 5
+	output=`qstat $jobid`
+	rc=$?
+done
+
+# look for return code
+grep "exit code of 0" $$-tmp.debuglog
+if [ $? -ne 0 ]; then
+	exit 1
+else
+	exit 0
+fi
