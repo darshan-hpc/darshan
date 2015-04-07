96df671a91a1f0e0dd867943da1fc39cdc002249
Phil Carns <carns@mcs.anl.gov>
2015-04-07 12:46:41 -0400

    check mpi counters in mpi-io-test regression

--- darshan-test/regression/test-cases/mpi-io-test.sh
+++ darshan-test/regression/test-cases/mpi-io-test.sh
@@ -26,25 +26,22 @@ if [ $? -ne 0 ]; then
     echo "Error: failed to parse ${DARSHAN_LOGFILE}" 1>&2
     exit 1
 fi
+$DARSHAN_PATH/bin/darshan-mpiio-parser $DARSHAN_LOGFILE > $DARSHAN_TMP/${PROG}-mpiio.darshan.txt
+if [ $? -ne 0 ]; then
+    echo "Error: failed to parse ${DARSHAN_LOGFILE}" 1>&2
+    exit 1
+fi
 
-# check results
-# in this case we want to confirm that both the MPI and POSIX open counters were triggered
-#MPI_OPENS=`grep CP_INDEP_OPENS $DARSHAN_TMP/${PROG}.darshan.txt |cut -f 4`
-#if [ ! $MPI_OPENS -gt 0 ]; then
-#    echo "Error: MPI open count of $MPI_OPENS is incorrect" 1>&2
-#    exit 1
-#fi
-#POSIX_OPENS=`grep CP_POSIX_OPENS $DARSHAN_TMP/${PROG}.darshan.txt |cut -f 4`
-#if [ ! $POSIX_OPENS -gt 0 ]; then
-#    echo "Error: POSIX open count of $POSIX_OPENS is incorrect" 1>&2
-#    exit 1
-#fi
 POSIX_OPENS=`grep POSIX_OPENS $DARSHAN_TMP/${PROG}.darshan.txt |cut -d : -f 2 |xargs`
 if [ ! $POSIX_OPENS -gt 0 ]; then
     echo "Error: POSIX open count of $POSIX_OPENS is incorrect" 1>&2
     exit 1
 fi
-
+MPI_OPENS=`grep INDEP_OPENS $DARSHAN_TMP/${PROG}-mpiio.darshan.txt |cut -d : -f 2 |xargs`
+if [ ! $MPI_OPENS -gt 0 ]; then
+    echo "Error: MPI open count of $MPI_OPENS is incorrect" 1>&2
+    exit 1
+fi
 
 
 exit 0
