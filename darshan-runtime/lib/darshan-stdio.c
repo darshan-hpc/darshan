3b4bef1d3eb23d74a7e20c9dbcb1fad9e1632d5a
Phil Carns <carns@mcs.anl.gov>
2016-05-23 22:54:37 -0400

    fputs wrapper

--- darshan-runtime/lib/darshan-stdio.c
+++ darshan-runtime/lib/darshan-stdio.c
@@ -50,7 +50,7 @@
  * int      fprintf(FILE *, const char *, ...);             DONE
  * int      vfprintf(FILE *, const char *, va_list);        DONE
  * int      fputc(int, FILE *);                             DONE
- * int      fputs(const char *, FILE *);
+ * int      fputs(const char *, FILE *);                    DONE
  * size_t   fwrite(const void *, size_t, size_t, FILE *);   DONE
  * int      putc(int, FILE *);
  * int      putw(int, FILE *);
@@ -103,6 +103,7 @@ DARSHAN_FORWARD_DECL(fclose, int, (FILE *fp));
 DARSHAN_FORWARD_DECL(fflush, int, (FILE *fp));
 DARSHAN_FORWARD_DECL(fwrite, size_t, (const void *ptr, size_t size, size_t nmemb, FILE *stream));
 DARSHAN_FORWARD_DECL(fputc, int, (int c, FILE *stream));
+DARSHAN_FORWARD_DECL(fputs, int, (const char *s, FILE *stream));
 DARSHAN_FORWARD_DECL(fprintf, int, (FILE *stream, const char *format, ...));
 DARSHAN_FORWARD_DECL(vfprintf, int, (FILE *stream, const char *format, va_list));
 DARSHAN_FORWARD_DECL(fread, size_t, (void *ptr, size_t size, size_t nmemb, FILE *stream));
@@ -436,6 +437,27 @@ int DARSHAN_DECL(fputc)(int c, FILE *stream)
     stdio_runtime_initialize();
     if(ret != EOF)
         STDIO_RECORD_WRITE(stream, 1, tm1, tm2, 0);
+    STDIO_UNLOCK();
+
+    return(ret);
+}
+
+
+int DARSHAN_DECL(fputs)(const char *s, FILE *stream)
+{
+    int ret;
+    double tm1, tm2;
+
+    MAP_OR_FAIL(fputs);
+
+    tm1 = darshan_core_wtime();
+    ret = __real_fputs(s, stream);
+    tm2 = darshan_core_wtime();
+
+    STDIO_LOCK();
+    stdio_runtime_initialize();
+    if(ret != EOF && ret > 0)
+        STDIO_RECORD_WRITE(stream, strlen(s), tm1, tm2, 0);
     STDIO_UNLOCK();
 
     return(ret);
