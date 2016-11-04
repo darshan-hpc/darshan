ed51f8b813715d223ae336df49361f6e88f94222
Shane Snyder <ssnyder@mcs.anl.gov>
2016-11-04 13:06:37 -0500

    update dynamic regression scripts to test ls cmd

--- darshan-test/regression/run-all.sh
+++ darshan-test/regression/run-all.sh
@@ -45,17 +45,6 @@ fi
 # set up environment for tests according to platform
 source $DARSHAN_TESTDIR/$DARSHAN_PLATFORM/env.sh
 
-# in case of using LD_PRELOAD, it is possible errors with Darshan symbols
-# (e..g, forgetting to resolve a symbol Darshan wraps using dlsym) can
-# cause arbitrary binaries to crash. We check the output of the true
-# command to successfully identify failures like this and exit
-true_out=`/bin/true 2>&1`
-if [ $? -ne 0 -o -n "$true_out" ]; then
-    echo -n $true_out > tmp
-    echo "environment setup failed"
-    exit 1
-fi
-
 failure_count=0
 
 for i in `ls $DARSHAN_TESTDIR/test-cases/*.sh`; do
--- darshan-test/regression/workstation-dynamic/env.sh
+++ darshan-test/regression/workstation-dynamic/env.sh
@@ -38,3 +38,20 @@ FULL_MPICC_PATH=`which mpicc`
 export LD_PRELOAD=`dirname $FULL_MPICC_PATH`/../lib/libfmpich.so:$DARSHAN_PATH/lib/libdarshan.so:$LD_PRELOAD
 
 export DARSHAN_RUNJOB="mpiexec -n $DARSHAN_DEFAULT_NPROCS"
+
+# in case of using LD_PRELOAD, it is possible errors with Darshan symbols
+# (e..g, forgetting to resolve a symbol Darshan wraps using dlsym) can
+# cause arbitrary binaries to crash. We check the output of the true
+# command and ls to try to identify failures like this and exit
+true_out=`true`
+if [ $? -ne 0 -o -n "$true_out" ]; then
+    echo -n "$true_out"
+    echo "true command test failed"
+    exit 1
+fi
+ls_out=`ls 1>/dev/null`
+if [ $? -ne 0 -o -n "$ls_out" ]; then
+    echo -n "$ls_out"
+    echo "ls command test failed"
+    exit 1
+fi
