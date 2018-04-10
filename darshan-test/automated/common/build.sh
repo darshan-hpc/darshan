2ffc358418f23149c3259f9506bc53b1d1dbd2b8
Shane Snyder <ssnyder@mcs.anl.gov>
2018-04-10 17:38:24 -0500

    more automated build refactoring

--- darshan-test/automated/common/build.sh
+++ darshan-test/automated/common/build.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 #
-# Base build script which calls the system specific version.
+# Build darshan runtime and util code
 #
+fcount=0
+runtime_status=0
+util_status=0
+runtime_result=""
+util_result=""
+thedate=$(date)
 
-status=0
-
-if [[ $NODE_LABELS =~ "mcs" ]];
-then
-  source darshan-test/automated/mcs/build.sh
-  status=$?
-else
-  # unknown machine
-  status=100
+cd build/darshan-runtime
+make && make install
+runtime_status=$?
+if [ $runtime_status -ne 0 ]; then
+  fcount=$((fcount+1));
+  runtime_result="<error type='$runtime_status' message='build failed' />"
 fi
 
-exit $status
+cd ../../build/darshan-util
+make && make install
+util_status=$?
+if [ $util_status -ne 0 ]; then
+  fcount=$((fcount+1));
+  util_result="<error type='$util_status' message='build failed' />"
+fi
+
+cd ../../;
+
+echo "
+<testsuites>
+  <testsuite name='build' tests='2' failures='$fcount' time='$thedate'>
+    <testcase name='darshan-runtime' time='$thedate'>
+    $runtime_result
+    </testcase>
+    <testcase name='darshan-util' time='$thedate'>
+    $util_result
+    </testcase>
+  </testsuite>
+</testsuites>
+" > build-result.xml
+
+exit $fcount
