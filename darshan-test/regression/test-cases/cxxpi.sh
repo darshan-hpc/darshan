e65be62d315fb278a6837dcb8e21f013888aeead
Shane Snyder <ssnyder@mcs.anl.gov>
2019-01-22 11:29:59 -0600

    update regression tests to avoid comment lines

--- darshan-test/regression/test-cases/cxxpi.sh
+++ darshan-test/regression/test-cases/cxxpi.sh
@@ -29,7 +29,7 @@ fi
 
 # check results
 # in this case we want to confirm that the open counts are zero; cxxpi does not do any IO
-POSIX_OPENS=`grep POSIX_OPENS $DARSHAN_TMP/${PROG}.darshan.txt |cut -f 5`
+POSIX_OPENS=`grep POSIX_OPENS $DARSHAN_TMP/${PROG}.darshan.txt |grep -vE "^#" |cut -f 5`
 if [ "$POSIX_OPENS"x != ""x ]; then
     echo "Error: Found unexpected POSIX open count of $POSIX_OPENS" 1>&2
     exit 1
