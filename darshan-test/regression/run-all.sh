615948412195887179750ff749323161ebf47472
Shane Snyder <ssnyder@mcs.anl.gov>
2020-04-27 09:54:11 -0500

    restrict number of test cases for now

--- darshan-test/regression/run-all.sh
+++ darshan-test/regression/run-all.sh
@@ -47,7 +47,8 @@ source $DARSHAN_TESTDIR/$DARSHAN_PLATFORM/env.sh
 
 failure_count=0
 
-for i in `ls $DARSHAN_TESTDIR/test-cases/*.sh`; do
+#for i in `ls $DARSHAN_TESTDIR/test-cases/*.sh`; do
+for i in `ls $DARSHAN_TESTDIR/test-cases/mpi-io-test.sh`; do
     echo Running ${i}...
     $i
     if [ $? -ne 0 ]; then
