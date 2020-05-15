f7b2b8fdaa6713be1e61e444e6ce7e34f6780be2
Shane Snyder <ssnyder@mcs.anl.gov>
2020-05-15 10:10:42 -0500

    bug fix for common access counter updates

--- darshan-runtime/darshan-common.h
+++ darshan-runtime/darshan-common.h
@@ -95,7 +95,7 @@
     if(*(int64_t *)__vals == 0) break; \
     for(i_=0; i_<4; i_++) { \
         if(__add_flag && \
-            !memcmp(__val_p + (i_ * sizeof(*__vals) * __val_size), \
+            !memcmp(__val_p + (i_ * __val_size), \
                 __vals, sizeof(*__vals) * __val_size)) { \
             total_count += *(__cnt_p + i_); \
             break; \
@@ -107,8 +107,8 @@
     for(i_=0; i_ < 4; i_++) { \
         if((*(__cnt_p + i_) > total_count) || \
            ((*(__cnt_p + i_) == total_count) && \
-           (*(__val_p + (i_ * sizeof(*__vals) * __val_size)) > *(int64_t *)__vals))) { \
-            memcpy(&tmp_val[tmp_ndx * __val_size], __val_p + (i_ * sizeof(*__vals) * __val_size), \
+           (*(__val_p + (i_ * __val_size)) > *(int64_t *)__vals))) { \
+            memcpy(&tmp_val[tmp_ndx * __val_size], __val_p + (i_ * __val_size), \
                 sizeof(*__vals) * __val_size); \
             tmp_cnt[tmp_ndx] = *(__cnt_p + i_); \
             tmp_ndx++; \
@@ -122,9 +122,9 @@
     tmp_ndx++; \
     /* last, copy over any remaining counters to make sure we have 4 sets total */ \
     while(tmp_ndx != 4) { \
-        if(memcmp(__val_p + (i_ * sizeof(*__vals) * __val_size), \
+        if(memcmp(__val_p + (i_ * __val_size), \
                 __vals, sizeof(*__vals) * __val_size)) { \
-            memcpy(&tmp_val[tmp_ndx * __val_size], __val_p + (i_ * sizeof(*__vals) * __val_size), \
+            memcpy(&tmp_val[tmp_ndx * __val_size], __val_p + (i_ * __val_size), \
                 sizeof(*__vals) * __val_size); \
             tmp_cnt[tmp_ndx] = *(__cnt_p + i_); \
             tmp_ndx++; \
