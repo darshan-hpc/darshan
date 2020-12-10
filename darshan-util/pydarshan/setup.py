24029dccc68ffaea571cbc86199d4528a1fc5caa
Shane Snyder <ssnyder@mcs.anl.gov>
2020-12-10 14:10:57 -0600

    updated pydarshan versions for latest tests

--- darshan-util/pydarshan/setup.py
+++ darshan-util/pydarshan/setup.py
@@ -68,6 +68,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://www.mcs.anl.gov/research/projects/darshan/',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
