8429a8e5c2821bdf0c15961bfa6d81434b60ba1b
Shane Snyder <ssnyder@mcs.anl.gov>
2021-01-13 21:07:43 -0600

    bumped pydarshan versions for new pypi release

--- darshan-util/pydarshan/setup.py
+++ darshan-util/pydarshan/setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://www.mcs.anl.gov/research/projects/darshan/',
-    version='0.0.4',
+    version='0.0.5',
     zip_safe=False,
 )
