6cc66ff4b9c3596a8a0cbe348cd933b90b30a7f7
Kevin Harms <harms@alcf.anl.gov>
2021-02-25 15:50:34 +0000

    Fix aggregators documentation generation

--- darshan-util/pydarshan/darshan/experimental/aggregators/__init__.py
+++ darshan-util/pydarshan/darshan/experimental/aggregators/__init__.py
@@ -1,12 +1,16 @@
 """Aggregator functions are monkey patched into the DarshanReport object during
-initialization of DarshanReport class. These functions are only enabled if the
-`darshan.enable_experimental(True)` function is called.
+initialization of DarshanReport class.
 
-Example::
+.. note::
+   These functions are only enabled if the `darshan.enable_experimental(True)` function is called.
+
+Example usage::
+
   import darshan
   import darshan.report
   dasrhan.enable_experimental(True)
   report = darshan.report.DarshanReport()
   ...
   result = report.agg_ioops()
+
 """
