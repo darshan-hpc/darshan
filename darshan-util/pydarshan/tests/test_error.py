541f82bba53fb5dd4504ea9c5fe53bb7dd0f80dd
Shane Snyder <ssnyder@mcs.anl.gov>
2021-05-11 21:49:30 -0500

    modify test to check correct return values

--- darshan-util/pydarshan/tests/test_error.py
+++ darshan-util/pydarshan/tests/test_error.py
@@ -7,24 +7,14 @@ import pytest
 
 import darshan.backend.cffi_backend as backend
 
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    pass
-
-
 def test_cannotopen():
     """Ensure we do not dump core on error."""
 
+    # assert fake log file name comes back with a NULL file handle
     log = backend.log_open("fake/tooth-fairy.darshan")
+    c_file_handle_repr = log['handle'].__repr__()
+    assert 'NULL' in c_file_handle_repr
 
+    # assert no record is returned using above NULL file handle
     rec = backend.log_get_record(log, "MPI-IO")
-    assert rec['counters'][1] == 2048
-
-
-def test_ishouldrun():
-    assert 1
+    assert rec == None
