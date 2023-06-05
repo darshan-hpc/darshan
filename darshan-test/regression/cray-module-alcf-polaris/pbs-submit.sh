bc14fdac6abd9af4c6c9ca9932dcd02966a7de13
Shane Snyder <ssnyder@mcs.anl.gov>
2023-06-05 19:47:43 +0000

    remove debugging helpers

--- darshan-test/regression/cray-module-alcf-polaris/pbs-submit.sh
+++ darshan-test/regression/cray-module-alcf-polaris/pbs-submit.sh
@@ -4,7 +4,7 @@ nprocs=$DARSHAN_DEFAULT_NPROCS
 nnodes=`wc -l < $PBS_NODEFILE`
 ppn=$((nprocs / nnodes))
 
-mpiexec -n $nprocs --ppn $ppn --env LD_DEBUG=all $DARSHAN_SCRIPT_ARGS
+mpiexec -n $nprocs --ppn $ppn $DARSHAN_SCRIPT_ARGS
 EXIT_STATUS=$?
 
 exit $EXIT_STATUS
