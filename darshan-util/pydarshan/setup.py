b89f6db81f0fffb21a817113531383b0487895dd
Shane Snyder <ssnyder@mcs.anl.gov>
2023-12-15 15:50:41 -0600

    first attempt at pyproject.toml [wheel build]

--- darshan-util/pydarshan/setup.py
+++ darshan-util/pydarshan/setup.py
@@ -9,17 +9,6 @@ if sys.version_info[:2] < (3, 7):
     raise RuntimeError("Python version >= 3.7 required.")
 
 
-with open("README.rst") as readme_file:
-    readme = readme_file.read()
-
-
-requirements = ["cffi", "numpy", "pandas", "matplotlib", "seaborn", "mako", "humanize"]
-
-setup_requirements = [
-    "pytest-runner",
-]
-test_requirements = ["pytest"]
-
 
 # NOTE: The Python C extension is currently only used to automate
 # the build process of binary wheels for distribution via PyPi.
@@ -54,33 +43,9 @@ for root, dirs, files in os.walk("../../modules"):
 
 
 setup(
-    author="",
-    author_email="",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-    ],
-    description="Python tools to interact with darshan log records of HPC applications.",
-    long_description=readme,
     ext_modules=ext_modules,
-    install_requires=requirements,
     include_package_data=True,
-    keywords="darshan",
-    name="darshan",
     packages=find_packages(include=["darshan"]),
-    setup_requires=setup_requirements,
-    tests_require=test_requirements,
-    url='https://www.mcs.anl.gov/research/projects/darshan/',
-    version='3.4.4.0',
     zip_safe=False,
     package_data={"": ["*.darshan"],
                   "darshan": ["cli/style.css",
