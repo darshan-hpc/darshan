7a1faa0b0449aa533e1158dec6e563fa46e96328
wkliao <wkliao@users.noreply.github.com>
2025-10-04 13:48:37 -0500

    build runtime and util separately
    When building, run configure from the root folder.

--- darshan-test/automated/build-darshan.sh
+++ darshan-test/automated/build-darshan.sh
@@ -23,18 +23,18 @@ mkdir -p $DARSHAN_LOG_PATH
 
 # configure and build darshan-runtime (if not requested to skip)
 if [ ! -v DARSHAN_RUNTIME_SKIP ]; then
-    mkdir -p $build_dir/darshan-runtime && cd $build_dir/darshan-runtime
+    mkdir -p $build_dir && cd $build_dir
     if [ -z "${DARSHAN_RUNTIME_CONFIG_ARGS}" ]; then
         DARSHAN_RUNTIME_CONFIG_ARGS="--with-jobid-env=NONE --enable-apmpi-mod"
     fi
-    $darshan_root_dir/darshan-runtime/configure $DARSHAN_RUNTIME_CONFIG_ARGS --with-log-path=$DARSHAN_LOG_PATH --prefix=$DARSHAN_INSTALL_PREFIX
-    make install
+    $darshan_root_dir/configure --disable-darshan-util $DARSHAN_RUNTIME_CONFIG_ARGS --with-log-path=$DARSHAN_LOG_PATH --prefix=$DARSHAN_INSTALL_PREFIX
+    make -j8 install
 fi
 
 # configure and build darshan-util
-mkdir -p $build_dir/darshan-util && cd $build_dir/darshan-util
+mkdir -p $build_dir && cd $build_dir
 if [ -z "${DARSHAN_UTIL_CONFIG_ARGS}" ]; then
     DARSHAN_UTIL_CONFIG_ARGS="--enable-apmpi-mod --enable-apxc-mod"
 fi
-$darshan_root_dir/darshan-util/configure $DARSHAN_UTIL_CONFIG_ARGS --prefix=$DARSHAN_INSTALL_PREFIX
-make install
+$darshan_root_dir/configure --disable-darshan-runtime $DARSHAN_UTIL_CONFIG_ARGS --prefix=$DARSHAN_INSTALL_PREFIX
+make -j8 install
